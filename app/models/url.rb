667033889ac077fd3ba893df808331e121128611
Tom Uebel <tauebel@gmail.com>
2013-01-31 04:14:21 -0800

    updated styling and added url validation

--- app/models/url.rb
+++ app/models/url.rb
@@ -2,6 +2,8 @@ class Url < ActiveRecord::Base
   before_create :get_shortened_url
   attr_accessible :original_url, :shortened_url
   
+  validates :original_url, :format => { :with => /https?:\/\/[\S]+/, :message => "Your URL must begin with 'http:// or https://"}
+  
   private
   
   def get_shortened_url
